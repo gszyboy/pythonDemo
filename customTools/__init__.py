b7c9d2fab156b404693bfd18b4698e388492f96b
gszyboy <a0936@163.com>
2019-01-10 09:37:25 +0800

    新增

---
+++ customTools/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Date    : 2019-01-09 09:32:42
+# @Author  : 如风 (26982310@qq.com)
+# @Link    : http://blog.youmaku.top
+# @Version : $Id$
+'''
+自己常用的工具包
+'''
+__all__ = ['fetchDocx','thulacTool']
---
+++ customTools/thulacTool.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Date    : 2019-01-09 09:37:59
+# @Author  : 如风 (26982310@qq.com)
+# @Link    : http://blog.youmaku.top
+# @Version : $Id$
+
+import thulac
+
+class Thulacp(object):
+    """THULAC:一个高效的中文词法分析工具包"""
+    def __init__(self, arg):
+        super(Thulacp, self).__init__()
+        self.arg = arg
+
+    def abc(self):
+        pass
+
+thul = thulac.thulac()
+text =''
+
+
